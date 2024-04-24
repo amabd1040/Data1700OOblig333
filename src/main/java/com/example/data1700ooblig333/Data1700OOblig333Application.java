d67dd51943b1712085dbb5cde66ff3694a2f12ae
amabd1040 <amabd1040@oslomet.no>
2024-04-24 19:27:06 +0200

    Initial commit

---
+++ src/main/java/com/example/data1700ooblig333/Data1700OOblig333Application.java
@@ -0,0 +1,13 @@
+package com.example.data1700ooblig333;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Data1700OOblig333Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Data1700OOblig333Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/data1700ooblig333/Data1700OOblig333ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.data1700ooblig333;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Data1700OOblig333ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
